8b6894ae231c6ded236632714c775ea50c2f59f5
JuanG <juanguillermocrespovelez61@gmail.com>
2022-11-08 20:34:38 -0500

    Diseño y configuración del Activity Recuperar

--- app/src/main/java/com/juang/smallfeet/view/ui/activities/RecuperarActivity.kt
+++ app/src/main/java/com/juang/smallfeet/view/ui/activities/RecuperarActivity.kt
@@ -1,12 +1,19 @@
 package com.juang.smallfeet.view.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.juang.smallfeet.R
 
 class RecuperarActivity : AppCompatActivity() {
+    lateinit var restaurarbutton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recuperar)
+        restaurarbutton=findViewById(R.id.btrestaurar)
+        restaurarbutton.setOnClickListener{
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 }
